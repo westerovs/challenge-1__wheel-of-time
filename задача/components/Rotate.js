57aba10e9c5f3da9a67af7d4f13fb826307f0efe
Александр <westerovs@mail.ru>
2022-10-22 23:52:50 +0300

    update readmy

--- задача/components/Rotate.js
+++ задача/components/Rotate.js
@@ -1,3 +1,6 @@
+// Для запуска проекта, достаточно запустить любой live server,
+// например в vs code, или в webstorm
+
 export default class RotateElement {
   constructor() {
     this.container = document.querySelector('.container__inner')
