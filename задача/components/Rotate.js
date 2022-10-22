c70cdef40247a23eb24192c3b49539606b38535c
Александр <westerovs@mail.ru>
2022-10-22 23:23:27 +0300

    update

--- задача/components/Rotate.js
+++ задача/components/Rotate.js
@@ -1,13 +1,12 @@
 export default class RotateElement {
   constructor() {
     this.container = document.querySelector('.container__inner')
-    
 
     this.init()
   }
   
   init = () => {
-    console.log('Ваше решение тут')
+    console.log('Ваш код...')
   }
   
   // Ваш код...
